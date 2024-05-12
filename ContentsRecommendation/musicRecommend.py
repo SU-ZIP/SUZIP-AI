bb9d5635838692e67249d4201ad54d63253a6ac4
hypoxisaurea <flickerlight14@naver.com>
2024-05-12 20:47:46 +0900

    [Feat] Update Recommend Function

--- ContentsRecommendation/musicRecommend.py
+++ ContentsRecommendation/musicRecommend.py
@@ -3,10 +3,11 @@ import random
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # 환경 변수 불러오기
-
+# 환경 변수 불러오기
+load_dotenv()
 client_id = os.getenv('SPOTIFY_CLIENT_ID')
 client_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
+
 
 def get_spotify_token():
     url = "https://accounts.spotify.com/api/token"
@@ -16,6 +17,7 @@ def get_spotify_token():
         'client_secret': client_secret
     })
     return response.json()['access_token']
+
 
 def fetch_music_recommendation(emotion):
     genre_map = {
@@ -29,6 +31,7 @@ def fetch_music_recommendation(emotion):
     token = get_spotify_token()
     headers = {'Authorization': f'Bearer {token}'}
 
+
     # 감정에 맞는 키워드로 플레이리스트 검색, 배열 내 요소를 OR 연산자로 결합
     keywords = genre_map.get(emotion, ['music'])
     query = " OR ".join(keywords)
@@ -36,12 +39,15 @@ def fetch_music_recommendation(emotion):
     response = requests.get(url, headers=headers)
     playlists = response.json().get('playlists', {}).get('items', [])
     
+
     if not playlists:
         return "No playlists found."
+
 
     # 첫 번째 플레이리스트 선택
     playlist_id = playlists[0]['id']
     
+
     # 플레이리스트의 트랙 가져오기
     tracks_url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
     tracks_response = requests.get(tracks_url, headers=headers)
@@ -50,6 +56,7 @@ def fetch_music_recommendation(emotion):
     if not tracks:
         return "No tracks found in the playlist."
     
+
     # 랜덤 트랙 선택
     track = random.choice(tracks)['track']
     artists_names = ', '.join(artist['name'] for artist in track['artists'])
@@ -60,7 +67,9 @@ def fetch_music_recommendation(emotion):
         "artist": artists_names
     }
 
+
     return custom_data
+
 
 # 감정 입력 및 음악 추천 실행
 emotion = '기쁨'
---
+++ recommendContents.py
@@ -0,0 +1,18 @@
+import json
+from ContentsRecommendation import movieRecommend, musicRecommend, bookRecommend
+
+def recommend_contents(emotion):
+    movie = movieRecommend.recommend_movie(emotion)
+    music = musicRecommend.recommend_music(emotion)
+    book = bookRecommend.recommend_book()
+    
+    recommendations = {
+        "movie": movie,
+        "music": music,
+        "book": book
+    }
+    
+    # 딕셔너리를 JSON 문자열로 변환
+    recommendations_json = json.dumps(recommendations, ensure_ascii=False, indent=4)
+
+    return recommendations_json
